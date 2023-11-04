1069140e589df1a29c2aa3561820ffbf77c94292
SJSuhas <sjsuhas101@gmail.com>
2023-11-04 19:17:42 +0530

    initial commit

---
+++ Register.js
@@ -0,0 +1,9 @@
+ // Get the "Register" button by its Class Name
+    var regbtn1 = document.getElementsByClassName("register-link");
+
+    // Add a click event listener to the button
+    regbtn1.addEventListener("click", function() {
+// Redirect to the desired page when the button is clicked
+        window.location.href = "Signup.html";
+    });
+
